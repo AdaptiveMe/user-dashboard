c6283ba3b2747f67cfbc8237703627acad8198f7
hectorrosell <hrrl@gft.com>
2015-08-05 13:31:00 +0200

    Status page, dashbar page

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -129,8 +129,6 @@ var Index = function () {
                 }
             }
 
-
-
             $('#calendar').fullCalendar('destroy'); // destroy the calendar
             $('#calendar').fullCalendar({ //re-initialize the calendar
                 disableDragging : false,
@@ -223,33 +221,34 @@ var Index = function () {
                 return (Math.floor(Math.random() * (1 + 50 - 20))) + 10;
             }
 
-            var visitors = [
-                ['02/2013', 1500],
-                ['03/2013', 2500],
-                ['04/2013', 1700],
-                ['05/2013', 800],
-                ['06/2013', 1500],
-                ['07/2013', 2350],
-                ['08/2013', 1500],
-                ['09/2013', 1300],
-                ['10/2013', 4600]
+            var data1 = [
+                [0, 1500],
+                [1, 2500],
+                [3, 1700],
+                [4, 800],
+                [5, 1500],
+                [6, 2350],
+                [7, 1500],
+                [8, 1300],
+                [9, 4600]
             ];
 
-            if ($('#site_statistics').size() != 0) {
+            if ($('#site_statistics_android').size() != 0) {
 
-                $('#site_statistics_loading').hide();
-                $('#site_statistics_content').show();
+                $('#site_statistics_android_loading').hide();
 
-                var plot_statistics = $.plot($("#site_statistics"),
+                $('#site_statistics_android_content').show();
+
+                var plot_statistics = $.plot($("#site_statistics_android"),
                     [{
-                        data: visitors,
+                        data: data1,
                         lines: {
                             fill: 0.6,
                             lineWidth: 0
                         },
                         color: ['#f89f9f']
                     }, {
-                        data: visitors,
+                        data: data1,
                         points: {
                             show: true,
                             fill: true,
@@ -260,9 +259,9 @@ var Index = function () {
                         color: '#fff',
                         shadowSize: 0
                     }],
-
                     {
                         xaxis: {
+                            ticks: [[0,'00'],[1,'01'],[2,'02'],[3,'03'],[4,'04'],[5,'05'],[6,'06'],[7,'07'],[8,'08'],[9,'09']],
                             tickLength: 0,
                             tickDecimals: 0,
                             mode: "categories",
@@ -295,7 +294,8 @@ var Index = function () {
                     });
 
                 var previousPoint = null;
-                $("#site_statistics").bind("plothover", function (event, pos, item) {
+
+                $("#site_statistics_android").bind("plothover", function (event, pos, item) {
                     $("#x").text(pos.x.toFixed(2));
                     $("#y").text(pos.y.toFixed(2));
                     if (item) {
@@ -315,37 +315,36 @@ var Index = function () {
                 });
             }
 
-            if ($('#site_activities').size() != 0) {
+            if ($('#site_statistics_ios').size() != 0) {
                 //site activities
                 var previousPoint2 = null;
-                $('#site_activities_loading').hide();
-                $('#site_activities_content').show();
+                $('#site_statistics_ios_loading').hide();
+                $('#site_statistics_ios_content').show();
 
-                var data1 = [
-                    ['DEC', 300],
-                    ['JAN', 600],
-                    ['FEB', 1100],
-                    ['MAR', 1200],
-                    ['APR', 860],
-                    ['MAY', 1200],
-                    ['JUN', 1450],
-                    ['JUL', 1800],
-                    ['AUG', 1200],
-                    ['SEP', 600]
+                var data2 = [
+                    [0, 300],
+                    [1, 600],
+                    [2, 1100],
+                    [3, 1200],
+                    [4, 860],
+                    [5, 1200],
+                    [6, 1450],
+                    [7, 1800],
+                    [8, 1200],
+                    [9, 600]
                 ];
 
-
-                var plot_statistics = $.plot($("#site_activities"),
+                var plot_statistics_ios = $.plot($("#site_statistics_ios"),
 
                     [{
-                        data: data1,
+                        data: data2,
                         lines: {
                             fill: 0.2,
                             lineWidth: 0,
                         },
                         color: ['#BAD9F5']
                     }, {
-                        data: data1,
+                        data: data2,
                         points: {
                             show: true,
                             fill: true,
@@ -356,7 +355,7 @@ var Index = function () {
                         color: '#9ACAE6',
                         shadowSize: 1
                     }, {
-                        data: data1,
+                        data: data2,
                         lines: {
                             show: true,
                             fill: false,
@@ -369,6 +368,7 @@ var Index = function () {
                     {
 
                         xaxis: {
+                            ticks: [[0,'00'],[1,'01'],[2,'02'],[3,'03'],[4,'04'],[5,'05'],[6,'06'],[7,'07'],[8,'08'],[9,'09']],
                             tickLength: 0,
                             tickDecimals: 0,
                             mode: "categories",
@@ -400,7 +400,7 @@ var Index = function () {
                         }
                     });
 
-                $("#site_activities").bind("plothover", function (event, pos, item) {
+                $("#site_statistics_ios").bind("plothover", function (event, pos, item) {
                     $("#x").text(pos.x.toFixed(2));
                     $("#y").text(pos.y.toFixed(2));
                     if (item) {
@@ -414,7 +414,7 @@ var Index = function () {
                     }
                 });
 
-                $('#site_activities').bind("mouseleave", function () {
+                $('#site_statistics_ios').bind("mouseleave", function () {
                     $("#tooltip").remove();
                 });
             }
@@ -438,8 +438,8 @@ var Index = function () {
                         fNOP = function () {},
                         fBound = function () {
                             return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-                        aArgs.concat(Array.prototype.slice.call(arguments)));
-                    };
+                                aArgs.concat(Array.prototype.slice.call(arguments)));
+                        };
 
                     fNOP.prototype = this.prototype;
                     fBound.prototype = new fNOP();
@@ -621,7 +621,251 @@ var Index = function () {
 
             $('#dashboard-report-range span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
             $('#dashboard-report-range').show();
-        }
+        } ,
+
+        initDashbarCharts: function ()  {
+
+            if (!jQuery.plot) {
+                return;
+            }
+
+            function showChartTooltip(x, y, xValue, yValue) {
+                $('<div id="tooltip" class="chart-tooltip">' + yValue + '<\/div>').css({
+                    position: 'absolute',
+                    display: 'none',
+                    top: y - 40,
+                    left: x - 40,
+                    border: '0px solid #ccc',
+                    padding: '2px 6px',
+                    'background-color': '#fff'
+                }).appendTo("body").fadeIn(200);
+            }
+
+            var data = [];
+            var totalPoints = 250;
+
+            // random data generator for plot charts
+
+            function getRandomData() {
+                if (data.length > 0) data = data.slice(1);
+                // do a random walk
+                while (data.length < totalPoints) {
+                    var prev = data.length > 0 ? data[data.length - 1] : 50;
+                    var y = prev + Math.random() * 10 - 5;
+                    if (y < 0) y = 0;
+                    if (y > 100) y = 100;
+                    data.push(y);
+                }
+                // zip the generated y values with the x values
+                var res = [];
+                for (var i = 0; i < data.length; ++i) res.push([i, data[i]])
+                return res;
+            }
+
+            function randValue() {
+                return (Math.floor(Math.random() * (1 + 50 - 20))) + 10;
+            }
+
+            var androidBuildTime = [
+                [0, 1500],
+                [1, 2500],
+                [2, 1700],
+                [3, 800],
+                [4, 1500],
+                [5, 2350],
+                [6, 1500],
+                [7, 1300],
+                [8, 4600]
+            ];
+
+            if ($('#site_androidBuildTime').size() != 0) {
+
+                $('#site_androidBuildTime_loading').hide();
+                $('#site_androidBuildTime_content').show();
+
+                var plot_androidBuildTime = $.plot($("#site_androidBuildTime"),
+                    [{
+                        data: androidBuildTime,
+                        lines: {
+                            fill: 0.6,
+                            lineWidth: 0
+                        },
+                        color: ['#f89f9f']
+                    }, {
+                        data: androidBuildTime,
+                        points: {
+                            show: true,
+                            fill: true,
+                            radius: 5,
+                            fillColor: "#f89f9f",
+                            lineWidth: 3
+                        },
+                        color: '#fff',
+                        shadowSize: 0
+                    }],
+
+                    {
+                        xaxis: {
+                            ticks: [[0,'00'],[1,'01'],[2,'02'],[3,'03'],[4,'04'],[5,'05'],[6,'06'],[7,'07'],[8,'08'],[9,'09']],
+                            tickLength: 0,
+                            tickDecimals: 0,
+                            mode: "categories",
+                            min: 0,
+                            font: {
+                                lineHeight: 14,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        yaxis: {
+                            ticks: 5,
+                            tickDecimals: 0,
+                            tickColor: "#eee",
+                            font: {
+                                lineHeight: 14,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#eee",
+                            borderColor: "#eee",
+                            borderWidth: 1
+                        }
+                    });
+
+                var previousPoint = null;
+                $("#site_androidBuildTime").bind("plothover", function (event, pos, item) {
+                    $("#x").text(pos.x.toFixed(2));
+                    $("#y").text(pos.y.toFixed(2));
+                    if (item) {
+                        if (previousPoint != item.dataIndex) {
+                            previousPoint = item.dataIndex;
+
+                            $("#tooltip").remove();
+                            var x = item.datapoint[0].toFixed(2),
+                                y = item.datapoint[1].toFixed(2);
+
+                            showChartTooltip(item.pageX, item.pageY, item.datapoint[0], item.datapoint[1] + ' ...');
+                        }
+                    } else {
+                        $("#tooltip").remove();
+                        previousPoint = null;
+                    }
+                });
+            }
+
+
+            if ($('#site_iOSBuildTime').size() != 0) {
+                //site activities
+                var previousPoint2 = null;
+                $('#site_iOSBuildTime_loading').hide();
+                $('#site_iOSBuildTime_content').show();
+
+                var data1 = [
+                    [0, 300],
+                    [1, 600],
+                    [2, 1100],
+                    [3, 1200],
+                    [4, 860],
+                    [5, 1200],
+                    [6, 1450],
+                    [7, 1800],
+                    [8, 1200],
+                    [9, 600]
+                ];
+
+
+                var plot_iOSBuildTime = $.plot($("#site_iOSBuildTime"),
+
+                    [{
+                        data: data1,
+                        lines: {
+                            fill: 0.2,
+                            lineWidth: 0,
+                        },
+                        color: ['#BAD9F5']
+                    }, {
+                        data: data1,
+                        points: {
+                            show: true,
+                            fill: true,
+                            radius: 4,
+                            fillColor: "#9ACAE6",
+                            lineWidth: 2
+                        },
+                        color: '#9ACAE6',
+                        shadowSize: 1
+                    }, {
+                        data: data1,
+                        lines: {
+                            show: true,
+                            fill: false,
+                            lineWidth: 3
+                        },
+                        color: '#9ACAE6',
+                        shadowSize: 0
+                    }],
+
+                    {
+
+                        xaxis: {
+
+                            ticks: [[0,'00'],[1,'01'],[2,'02'],[3,'03'],[4,'04'],[5,'05'],[6,'06'],[7,'07'],[8,'08'],[9,'09']],
+                            tickLength: 0,
+                            tickDecimals: 0,
+                            mode: "categories",
+                            min: 0,
+                            font: {
+                                lineHeight: 18,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        yaxis: {
+                            ticks: 5,
+                            tickDecimals: 0,
+                            tickColor: "#eee",
+                            font: {
+                                lineHeight: 14,
+                                style: "normal",
+                                variant: "small-caps",
+                                color: "#6F7B8A"
+                            }
+                        },
+                        grid: {
+                            hoverable: true,
+                            clickable: true,
+                            tickColor: "#eee",
+                            borderColor: "#eee",
+                            borderWidth: 1
+                        }
+                    });
+
+                $("#site_iOSBuildTime").bind("plothover", function (event, pos, item) {
+                    $("#x").text(pos.x.toFixed(2));
+                    $("#y").text(pos.y.toFixed(2));
+                    if (item) {
+                        if (previousPoint2 != item.dataIndex) {
+                            previousPoint2 = item.dataIndex;
+                            $("#tooltip").remove();
+                            var x = item.datapoint[0].toFixed(2),
+                                y = item.datapoint[1].toFixed(2);
+                            showChartTooltip(item.pageX, item.pageY, item.datapoint[0], item.datapoint[1] + '...');
+                        }
+                    }
+                });
+
+                $('#site_iOSBuildTime').bind("mouseleave", function () {
+                    $("#tooltip").remove();
+                });
+            }
+        } ,
 
     };
 
